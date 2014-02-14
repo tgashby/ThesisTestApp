77cef784322f1c2f698c210ac7a0c7f7bb002e30
Tag Ashby <tgashby@calpoly.edu>
2014-02-14 08:57:51 -0800

    Still fighting with JS about this binding (at least I think that's the problem). Wanted to get a commmit in anyways.

--- spec/jasminespec.js
+++ spec/jasminespec.js
@@ -2,74 +2,108 @@ describe("Task Spec", function() {
     var tasks = require('../routes/tasks');
     var http = require('http');
     var mongoskin = require('mongoskin');
-    var db = mongoskin.db('mongodb://localhost:27017/todo?auto_reconnect', {safe:true});
+    var db = mongoskin.db('mongodb://localhost:27017/todo?auto_reconnect', {
+        safe: true
+    });
 
     var req = {};
     req.db = {};
     req.db.tasks = db.collection('tasks');
 
-    var res = ...
+    var res = {};
+    res.currPage = "UI HOME";
+    res.renderObj = {};
+    res.render = function(pageName, object) {
+        console.info("RENDER: " + pageName);
+        console.info(object);
 
-    var next = ...
+        this.currPage = pageName;
+        this.renderObj = object;
+    };
 
-    // Prepopulate DB
-    populateDB(req);
-    
+    res.redirect = function(pageName) {
+        console.info("REDIRECT: " + pageName);
+
+        this.currPage = pageName;
+    }
+
+    var next = function(error) {
+        console.info("ERROR: " + error);
+    }
+
+    beforeEach(function() {
+      populateDB(req, res, next);
+    });
+
+    afterEach(function() {
+      tearDownDB(req, res, next);
+    });
+
     it("should be able to list all tasks", function() {
-        tasks.list(...);
+        tasks.list(req, res, next);
 
-        expect(...).toBe(...);
+        console.info("Res after list:")
+        console.info(res)
+
+        expect(res.currPage).toBe("tasks");
+        expect(res.renderObj.tasks.length).toEqual(2);
     });
 
-    it("should be able to add a task", function() {
-        tasks.add(...);
+    // it("should be able to add a task", function() {
+    //     tasks.add(...);
 
-        expect(...).toBe(...);
-    });
+    //     expect(...).toBe(...);
+    // });
 
-    it("should be able to mark all tasks completed", function() {
-        tasks.markAllCompleted(...);
+    // it("should be able to mark all tasks completed", function() {
+    //     tasks.markAllCompleted(...);
 
-        expect(...).toBe(...);
-    });
+    //     expect(...).toBe(...);
+    // });
 
-    it("should be able to show all completed tasks", function() {
-        tasks.completed(...);
+    // it("should be able to show all completed tasks", function() {
+    //     tasks.completed(...);
 
-        expect(...).toBe(...);
-    });
+    //     expect(...).toBe(...);
+    // });
 
-    it("should be able to mark a single task as completed", function() {
-        tasks.markCompleted(...);
+    // it("should be able to mark a single task as completed", function() {
+    //     tasks.markCompleted(...);
 
-        expect(...).toBe(...);
-    });;
+    //     expect(...).toBe(...);
+    // });;
 
-    it("should be able to delete a task", function() {
-        tasks.del(...);
+    // it("should be able to delete a task", function() {
+    //     tasks.del(...);
 
-        expect(...).toBe(...);
-    });
+    //     expect(...).toBe(...);
+    // });
 });
 
-function populateDB (req, res, next) {
+function populateDB(req, res, next) {
     req.db.tasks.save({
-      name: "Task One",
-      completed: false
-    }, function(error, task){
-      if (error) return next(error);
-      if (!task) return next(new Error('Failed to save.'));
-      console.info('Added %s with id=%s', task.name, task._id);
-      // res.redirect('/tasks');
+        name: "Task One",
+        completed: false
+    }, function(error, task) {
+        if (error) return next(error);
+        if (!task) return next(new Error('Failed to save.'));
+        console.info('Added %s with id=%s', task.name, task._id);
+        // res.redirect('/tasks');
     });
 
     req.db.tasks.save({
-      name: "Task Two",
-      completed: false
-    }, function(error, task){
-      if (error) return next(error);
-      if (!task) return next(new Error('Failed to save.'));
-      console.info('Added %s with id=%s', task.name, task._id);
-      // res.redirect('/tasks');
+        name: "Task Two",
+        completed: false
+    }, function(error, task) {
+        if (error) return next(error);
+        if (!task) return next(new Error('Failed to save.'));
+        console.info('Added %s with id=%s', task.name, task._id);
+        // res.redirect('/tasks');
+    });
+}
+
+function tearDownDB(req, res, next) {
+    req.db.tasks.remove(function (error, tasks) {
+        console.info("Removed %s tasks.", tasks);
     });
 }

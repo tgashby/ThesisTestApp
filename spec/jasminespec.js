9b1a87fbd5a1b428a77ecff21f1e0aca373bc445
Tag Ashby <tgashby@calpoly.edu>
2014-02-17 21:34:48 -0800

    OMG Tests that actually work! :-O

--- spec/jasminespec.js
+++ spec/jasminespec.js
@@ -1,3 +1,16 @@
+function Flag() {
+    var flag = false;
+    var self = this;
+
+    this.isDone = function () {
+        return self.flag;
+    }
+
+    this.finish = function () {
+        self.flag = true;
+    }
+}
+
 describe("Task Spec", function() {
     var tasks = require('../routes/tasks');
     var http = require('http');
@@ -15,7 +28,6 @@ describe("Task Spec", function() {
     res.renderObj = {};
     res.render = function(pageName, object) {
         console.info("RENDER: " + pageName);
-        console.info(object);
 
         this.currPage = pageName;
         this.renderObj = object;
@@ -39,91 +51,230 @@ describe("Task Spec", function() {
       tearDownDB(req, res, next);
     });
 
-    xit("should be able to list all tasks", function() {
-        tasks.list(req, res, next);
+    it("should be able to list all tasks", function() {
 
-        console.info("Res after list:")
-        console.info(res)
+        // This is unfortunately better than the alternative of just guessing at a time.
+        // Have to make an object because there's no pointers in JS :-\
+        var flag = new Flag();
 
-        expect(res.currPage).toBe("tasks");
-        expect(res.renderObj.tasks.length).toEqual(2);
+        runs(function () {
+            tasks.list(req, res, next);
+
+            waitForDB(db, flag);
+        });
+
+        waitsFor(function () {
+            return flag.isDone();
+        }, "waiting for list to finish.", 500);
+
+        runs(function () {
+            expect(res.currPage).toBe("tasks");
+            expect(res.renderObj.tasks.length).toEqual(2);
+        });
     });
 
     it("should be able to add a task", function() {
         req.body = {};
         req.body.name = "Add Task";
 
-        console.info("Function call: " + new Date().getTime())
         tasks.add(req, res, next);
-        console.info("Post function call: " + new Date().getTime())
 
         var count = 0;
-        req.db.tasks.count({completed:false}, function(error, itemCount) {
-            if (error) return next(error);
-            
-            count = itemCount;
-            console.info("Inside count: " + new Date().getTime())
+        var flag = new Flag();
+        var flag2 = new Flag();
+
+        runs(function() {
+            req.db.tasks.count({completed:false}, function(error, itemCount) {
+                if (error) return next(error);
+                
+                count = itemCount;
+            });
+
+            waitForDB(db, flag);
+        })
+
+        waitsFor(function () {
+            return flag.isDone();
+        }, "The count should increase.", 500);
+
+        runs(function() {
+            expect(count).toEqual(3);
+
+            req.db.tasks.find({name:"Add Task"}).count(function(error, numTasks) {
+                count = numTasks;
+            });
+
+            waitForDB(db, flag2);
         });
+        
+        waitsFor(function () {
+            return flag2.isDone();
+        }, "The count should increase.", 500);
 
-        console.info("Test: " + new Date().getTime())
-        expect(count).toEqual(3);
-
-        req.db.tasks.find({name:"Add Task"}).count(function(error, numTasks) {
-            count = numTasks;
+        runs(function () {
+            expect(count).toEqual(1);
         });
-
-        expect(count).toEqual(1);
     });
 
-    // it("should be able to mark all tasks completed", function() {
-    //     tasks.markAllCompleted(...);
+    it("should be able to mark all tasks completed", function() {
+        var flag = new Flag();
+        var completedTaskNum = 0;
 
-    //     expect(...).toBe(...);
-    // });
+        req.body.all_done = "true";
 
-    // it("should be able to show all completed tasks", function() {
-    //     tasks.completed(...);
+        tasks.markAllCompleted(req, res, next);
+        
+        runs(function () {
+            req.db.tasks.find({completed: true}).count(function(error, numTasks) {
+                completedTaskNum = numTasks;
+            });
 
-    //     expect(...).toBe(...);
-    // });
+            waitForDB(db, flag);
+        });
 
-    // it("should be able to mark a single task as completed", function() {
-    //     tasks.markCompleted(...);
+        waitsFor(function () {
+            return flag.isDone();
+        }, "marking all tasks complete", 500);
 
-    //     expect(...).toBe(...);
-    // });;
+        runs(function () {
+            expect(completedTaskNum).toEqual(2);
+        });
+    });
 
-    // it("should be able to delete a task", function() {
-    //     tasks.del(...);
+    it("should be able to show all completed tasks", function() {
+        var setupFlag = new Flag();
+        var completedFuncFlag = new Flag();
+        var completedTasksNum = 0;
+        res.currPage = "BAD";
 
-    //     expect(...).toBe(...);
-    // });
+        runs(function () { 
+            req.db.tasks.findOne({completed: false}, function (error, task) {
+                req.db.tasks.updateById(task._id, {
+                    $set: {
+                        completed: true
+                    }
+                }, function (error, count) {
+                    completedTasksNum = count;
+                    console.info("Finished: " + new Date().getTime())
+                });
+            });
+
+            waitForDB(db, setupFlag);
+        });
+
+        
+        waitsFor(function () {
+            return setupFlag.isDone();
+        }, "setting up show completed tasks test", 500);
+
+        runs(function () {
+            console.info("Tests: " + new Date().getTime());
+
+            tasks.completed(req, res, next);
+
+            waitForDB(db, completedFuncFlag);
+        });
+
+        waitsFor(function () {
+            return completedFuncFlag.isDone();
+        }, "completed func call", 500);
+
+        runs(function () {
+            expect(res.renderObj.tasks.length).toEqual(1);
+            expect(res.currPage).toBe('tasks_completed');
+        })
+    });
+
+    it("should be able to mark a single task as completed", function() {
+        var getIdFlag = new Flag();
+        var markCompletedFlag = new Flag();
+        var countFlag = new Flag();
+
+        var count = 0;
+        req.task = {};
+
+        runs(function () {
+            req.db.tasks.findOne({completed: false}, function (error, task) {
+                req.body.completed = "true"
+
+                req.task._id = task._id;
+            });
+
+            waitForDB(db, getIdFlag);
+        });
+
+        waitsFor(function () {
+            return getIdFlag.isDone();
+        }, "get id in markCompleted", 500);
+
+        runs(function () { 
+            tasks.markCompleted(req, res, next);
+
+            waitForDB(db, markCompletedFlag);
+        });
+
+        waitsFor(function () {
+            return markCompletedFlag.isDone();
+        }, "markCompleted function call", 500);
+
+        runs(function () {
+            req.db.tasks.find({_id: req.task._id}).count(function (error,  numTasks) {
+                count = numTasks;
+            });
+
+            waitForDB(db, countFlag);
+        });
+
+        waitsFor(function() {
+            return countFlag.isDone();
+        }, "count in markCompleted", 500);
+
+        runs(function () {
+            expect(count).toBe(1);
+        });
+    });
+
+    it("should be able to delete a task", function() {
+        tasks.del(...);
+
+        expect(...).toBe(...);
+    });
+
+    // Nav tests!
 });
 
 function populateDB(req, res, next) {
-    req.db.tasks.save({
-        name: "Task One",
-        completed: false
-    }, function(error, task) {
-        if (error) return next(error);
-        if (!task) return next(new Error('Failed to save.'));
-        console.info('Added %s with id=%s', task.name, task._id);
-        // res.redirect('/tasks');
-    });
+    addTask("Task One", req, res, next);
+    addTask("Task Two", req, res, next);
+}
 
+function addTask(taskName, req, res, next) {
     req.db.tasks.save({
-        name: "Task Two",
+        name: taskName,
         completed: false
     }, function(error, task) {
         if (error) return next(error);
         if (!task) return next(new Error('Failed to save.'));
         console.info('Added %s with id=%s', task.name, task._id);
-        // res.redirect('/tasks');
     });
 }
 
 function tearDownDB(req, res, next) {
     req.db.tasks.remove(function (error, tasks) {
-        console.info("Removed %s tasks.", tasks);
+        console.info("\nRemoved %s tasks.\n", tasks);
+    });
+}
+
+function waitForDB(db, flagObj) {
+    db.collection('$cmd.sys.inprog').findOne(function (err, data) {
+        if (err) { throw err; }
+
+        if (data.inprog.length > 0) {
+            console.info("Actually waiting!")
+            setTimeout(function() { waitForDB(db, flag) }, 50);
+        }
+        else {
+            flagObj.finish();
+        }
     });
 }
